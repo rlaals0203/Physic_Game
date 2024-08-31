a40e28bc3714d6386d5cfa170c1ffbc8b8a43e0a
rlaals0203 <scientistmin@gmail.com>
2024-08-31 13:15:19 +0900

    feat : 7 new Quiz SO

--- Assets/01_Script/Player.cs
+++ Assets/01_Script/Player.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : PlayerMovement
 {
+    [SerializeField] private SpriteRenderer[] _quizSprite;
+    [SerializeField] private Quiz _quizCompo;
     public int _selected;
 
     private void Update()
@@ -21,10 +24,18 @@ public class Player : PlayerMovement
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag(""))
+        if (collision.CompareTag("Quiz") && _quizCompo.isStart)
         {
             _selected = int.Parse
-                (collision.name.ToString().Substring(collision.name.Length, 1));
+                (collision.name.ToString().Substring(collision.name.Length - 1, 1));
+
+            _quizSprite[_selected - 1].DOColor(Color.cyan, 0.5f);
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (_quizCompo.isStart)
+            _quizSprite[_selected - 1].DOColor(Color.white, 0.5f);
     }
 }
--- Assets/01_Script/Quiz.cs
+++ Assets/01_Script/Quiz.cs
@@ -8,40 +8,33 @@ using DG.Tweening;
 public class Quiz : MonoBehaviour
 {
     public static int currentLevel;
-    public static int correctCount;
+    public static int score;
 
     [SerializeField] private float _timeLimit;
 
     [SerializeField] private TextMeshProUGUI[] _optionTexts;
-    [SerializeField] private SpriteRenderer[] _quizBackgrounds;
+    [SerializeField] private SpriteRenderer[] _quizSprite;
     [SerializeField] private TextMeshProUGUI _title;
     [SerializeField] private TextMeshProUGUI _timerText;
+    [SerializeField] private TextMeshProUGUI _scoreText;
     [SerializeField] private Player _player;
 
-    private float _currentTime;
-    private bool isStart;
-
+    public bool isStart;
     public QuizSOList quizSOList;
+
+    private float _currentTime;
+
     private List<QuizSO> _quizList = new List<QuizSO>();
     private QuizSO _quiz;
 
     private void Awake()
     {
-        currentLevel = 0;
-        correctCount = 0;
-
-        for (int i = 0; i < 4; i++)
-        {
-            _quizBackgrounds[i]
-                = GameObject.Find($"Option{i + 1}").GetComponent<SpriteRenderer>();
-        }
-
         foreach (QuizSO quizSO in quizSOList.list)
         {
             _quizList.Add(quizSO);
         }
 
-        InitializeQuiz();
+        Restart();
     }
 
     private void Update()
@@ -49,9 +42,18 @@ public class Quiz : MonoBehaviour
         if (isStart)
         {
             _currentTime += Time.deltaTime;
-            _timerText.text = (_timeLimit - _currentTime).ToString("F2");
+            _timerText.text = $"{(_timeLimit - _currentTime).ToString("F2")}초";
             TimerCheck();
         }
+    }
+
+    private void Restart()
+    {
+        currentLevel = 0;
+        score = 0;
+        _scoreText.text = $"점수 : {score}";
+
+        InitializeQuiz();
     }
 
     private void InitializeQuiz()
@@ -63,10 +65,9 @@ public class Quiz : MonoBehaviour
         for (int i = 0; i < 4; i++)
         {
             _optionTexts[i].text = _quiz.optionText[i];
-            _quizBackgrounds[i].color = Color.white;
+            _quizSprite[i].color = Color.white;
         }
         _title.text = _quiz.titleText;
-
         isStart = true;
     }
 
@@ -74,15 +75,16 @@ public class Quiz : MonoBehaviour
     {
         if (_player._selected == _quiz.correctIndex)
         {
-            correctCount++;
+            score++;
+            _scoreText.text = $"점수 : {score}";
         }
 
         for (int i = 0; i < 4; i++)
         {
             if (i + 1 != _quiz.correctIndex)
-                _quizBackgrounds[i].DOColor(Color.red, 0.5f);
+                _quizSprite[i].DOColor(Color.red, 0.5f);
             else
-                _quizBackgrounds[i].DOColor(Color.green, 0.5f);
+                _quizSprite[i].DOColor(Color.green, 0.5f);
         }
 
         StartCoroutine(IntervalQuiz());
@@ -94,6 +96,7 @@ public class Quiz : MonoBehaviour
         {
             isStart = false;
             _currentTime = 0;
+            _timerText.text = $"{_currentTime}초";
 
             CheckAnswer();
         }
